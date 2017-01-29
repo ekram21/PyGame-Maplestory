1b5ca0c64909f02da25f3cc714b3096e2b2d7c6f
ekram21 <ekram_lol@hotmail.com>
2017-01-29 13:57:14 +0000

    Added collision line class

--- Background.py
+++ Background.py
@@ -53,3 +53,39 @@ class Background(pygame.sprite.Sprite):
         '''this function can ba called from outide to change the current animation sequence'''
 
         self.Current_Animation = anim
+
+
+
+
+class Collision_Line(pygame.sprite.Sprite):
+
+    def __init__(self, Start_X, Start_Y, width, height):
+        pygame.sprite.Sprite.__init__(self) #call Sprite intializer
+
+        self.image = pygame.Surface([width,height])
+        self.image.fill((255,255,255))
+
+        self.rect = self.image.get_rect()
+        self.rect.y = Start_Y
+        self.rect.x = Start_X
+
+
+
+class HenesysTiles(pygame.sprite.Sprite):
+    '''generates the tiles for henesys'''
+
+    def __init__(self, Start_X, Start_Y):
+        pygame.sprite.Sprite.__init__(self) #call Sprite intializer
+
+        self.image, self.rect = load_skill_image('enH0_2.png', 'MapData', 'Henesys')
+
+        self.Current_X_position = Start_X
+        self.Current_Y_position = Start_Y
+
+        self.rect.topleft = self.Current_X_position, self.Current_Y_position
+
+        self.Current_Animation = 'Idle'
+
+    def Update(self):
+
+        return None
